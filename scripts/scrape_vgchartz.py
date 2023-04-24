6a66634321f781c630cdea654e66cf9224fd025a
Adam Haertter <ah2005@engr.ship.edu>
2023-04-24 14:23:22 -0400

    Fetch table heads and code cleanup

--- scripts/scrape_vgchartz.py
+++ scripts/scrape_vgchartz.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 import csv
 
 url1 = "https://www.vgchartz.com/games/games.php?name=&keyword=&console=&region=All&developer=&publisher=&goty_year=&genre="
-url2 = "&boxart=Both&banner=Both&ownership=Both&showmultiplat=No&results=10000&order=Sales&showtotalsales=1&showpublisher=1&showvgchartzscore=0&showvgchartzscore=1shownasales=1&showdeveloper=1&showcriticscore=1&showpalsales=1&showreleasedate=1&showuserscore=1&showjapansales=1&showlastupdate=1&showothersales=1&showshipped=1"
+url2 = "&boxart=Both&banner=Both&ownership=Both&showmultiplat=No&results=10000&order=Sales&showtotalsales=1&showpublisher=1&showvgchartzscore=0&showvgchartzscore=1&shownasales=1&showdeveloper=1&showcriticscore=1&showpalsales=1&showreleasedate=1&showuserscore=1&showjapansales=1&showlastupdate=1&showothersales=1&showshipped=1"
 
 genres = ["Action", "Action-Adventure", "Adventure", "Board Game", "Education", "Fighting", "Misc", "MMO", "Music", "Party", "Platform", "Puzzle", "Racing", "Role-Playing", "Sandbox", "Shooter", "Simulation", "Sports", "Strategy", "Visual Novel"]
 
@@ -24,33 +24,25 @@ for genre in genres:
     print("Connecting to VGChartz Page for " + genre)
     url = url1 + genre.replace(' ', '+') + url2
     driver.get(url)
-    #driver.implicitly_wait(30)
     print("Driver got page content")
 
-    # html_content = driver.page_source
-    # print("Starting soup parser")
-    # soup = BeautifulSoup(html_content, 'html.parser')
-    # table = soup.find('table')
-
-    # Wait for the table element to be present
     table_xpath = '/html/body/div[4]/div/div[2]/table/tbody/tr/td/div/div[2]/table[1]'  # Replace with the XPath of your table
-    table_element = WebDriverWait(driver, 30).until(
+    table_element = WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, table_xpath))
     )
 
-    # Get the outerHTML of the table element
     table_html = table_element.get_attribute('outerHTML')
-
-    # Parse the HTML using BeautifulSoup
     soup = BeautifulSoup(table_html, 'html.parser')
-
     rows = soup.find_all('tr')
 
     print("Building data array from page...")
 
     data = []
-    for row in rows:
-        cols = row.find_all('td')
+    for i, row in enumerate(rows):
+        if(i < 3):
+            cols = row.find_all('th')
+        else:
+            cols = row.find_all('td')
         cols = [col.text.strip() for col in cols]
         data.append(cols)
 
