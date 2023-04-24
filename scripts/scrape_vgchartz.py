7e29ef88ec8dfe6eeeb66f89a93753b4d9fd4e8e
Adam Haertter <ah2005@engr.ship.edu>
2023-04-24 14:54:37 -0400

    Properly align column titles in csv writer

--- scripts/scrape_vgchartz.py
+++ scripts/scrape_vgchartz.py
@@ -9,7 +9,8 @@ import csv
 url1 = "https://www.vgchartz.com/games/games.php?name=&keyword=&console=&region=All&developer=&publisher=&goty_year=&genre="
 url2 = "&boxart=Both&banner=Both&ownership=Both&showmultiplat=No&results=10000&order=Sales&showtotalsales=1&showpublisher=1&showvgchartzscore=0&showvgchartzscore=1&shownasales=1&showdeveloper=1&showcriticscore=1&showpalsales=1&showreleasedate=1&showuserscore=1&showjapansales=1&showlastupdate=1&showothersales=1&showshipped=1"
 
-genres = ["Action", "Action-Adventure", "Adventure", "Board Game", "Education", "Fighting", "Misc", "MMO", "Music", "Party", "Platform", "Puzzle", "Racing", "Role-Playing", "Sandbox", "Shooter", "Simulation", "Sports", "Strategy", "Visual Novel"]
+#genres = ["Action", "Action-Adventure", "Adventure", "Board Game", "Education", "Fighting", "Misc", "MMO", "Music", "Party", "Platform", "Puzzle", "Racing", "Role-Playing", "Sandbox", "Shooter", "Simulation", "Sports", "Strategy", "Visual Novel"]
+genres = ["Action"]
 
 print("Initializing driver")
 driver_path = "scripts/chromedriver.exe" # Replace with path to your actual chromedriver
@@ -43,8 +44,16 @@ for genre in genres:
             cols = row.find_all('th')
         else:
             cols = row.find_all('td')
-        cols = [col.text.strip() for col in cols]
-        data.append(cols)
+        row_data = []
+        for col in cols:
+            if col.find('img') is not None:
+                row_data.append(col.find('img').get('alt'))
+            else:
+                row_data.append(col.text.strip())
+        if(i == 2):            
+            row_data.insert(1, "Box Art")
+            print(row_data)
+        data.append(row_data)
 
     output = "data/raw/" + genre.replace(' ', '_') + '.csv'
     print("Writing to file " + output)
